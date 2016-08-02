bc43955b5a2c3fd39825e62cca9ad568537ba732
gurujilove108 <dylan.dannenhauer@gmail.com>
2016-08-03 06:41:00 +0930

    made my site more responsive

--- static/js/index.js
+++ static/js/index.js
@@ -1,6 +1,5 @@
 jQuery(document).ready(function() {
     var currentWidth;
-    var logos;
     $(window).resize(onResize);
     function onResize(event) {
         currentWidth = jQuery(window).width();
@@ -8,6 +7,7 @@ jQuery(document).ready(function() {
             jQuery(".fa-3x").removeClass("fa-3x").addClass('fa-2x');
         } else if (currentWidth > 1300) {
             jQuery(".fa-2x").removeClass("fa-2x").addClass('fa-3x');
+            jQuery("#dropdownMenuButton i").removeClass("fa-3x").addClass("fa-2x");
         }
     }
     angular.bootstrap(document, ['dylansProfileApp']);
