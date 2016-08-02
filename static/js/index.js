aa106fc5b8d5f3978218231aac56789f40ee7760
gurujilove108 <dylan.dannenhauer@gmail.com>
2016-08-03 04:40:55 +0930

    got a design working that I'm comfortable with, about 90% responsive

--- static/js/index.js
+++ static/js/index.js
@@ -1,8 +1,15 @@
 jQuery(document).ready(function() {
-    // $(window).resize(onResize);
-    // function onResize(event) {
-    //     $("#wrapper").removeClass("toggled");
-    // }
+    var currentWidth;
+    var logos;
+    $(window).resize(onResize);
+    function onResize(event) {
+        currentWidth = jQuery(window).width();
+        if (currentWidth <= 1300) {
+            jQuery(".fa-3x").removeClass("fa-3x").addClass('fa-2x');
+        } else if (currentWidth > 1300) {
+            jQuery(".fa-2x").removeClass("fa-2x").addClass('fa-3x');
+        }
+    }
     angular.bootstrap(document, ['dylansProfileApp']);
 });
 
